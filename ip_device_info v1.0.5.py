acdf3cbfd10260cb9b6bb2c751df84b34c06ec94
Vladislav Minibaev <125858311+Bibosiandre@users.noreply.github.com>
2024-10-03 12:39:30 +0300

    Upload files

---
+++ ip_device_info v1.0.5.py
@@ -0,0 +1,339 @@
+# -*- coding: utf-8 -*-
+# IP Device info v1.0.5
+"""
+<parameters>
+    <title>IP Device info</title>
+    <version>1.0.5</version>
+
+    <parameter>
+        <id>COLLECT_DATA_FROM_DISABLED</id>
+        <type>boolean</type>
+        <name>Count off ip devices</name>
+        <value>False</value>
+    </parameter>
+
+    <parameter>
+        <id>ENABLE_SHOW_WEB</id>
+        <type>boolean</type>
+        <name>Output to the web interface</name>
+        <value>False</value>
+    </parameter>
+    <parameter>
+        <id>HOTKEY_SHOW_WEB</id>
+        <name>Key to output to the interface</name>
+        <type>string_from_list</type>
+        <value>F3</value>
+        <string_list>F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12</string_list>
+    </parameter>
+    <parameter>
+        <type>integer</type>
+        <id>MONITOR</id>
+        <name>Monitor for interface output</name>
+        <value>1</value>
+    </parameter>
+    <parameter>
+        <id>SAVE_DATA_XLSX</id>
+        <type>boolean</type>
+        <name>Saving file to xlsx</name>
+        <value>False</value>
+    </parameter>
+    <parameter>
+        <id>HOTKEY_DATA_XLSX</id>
+        <type>string_from_list</type>
+        <name>Key to save to xlsx</name>
+        <value>F5</value>
+        <string_list>F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12</string_list>
+    </parameter>
+    <parameter>
+        <id>DEBUG</id>
+        <type>boolean</type>
+        <name>Debug</name>
+        <value>False</value>
+    </parameter>
+
+    <resources>
+        <resource>export.py</resource>
+        <resource>helpers.py</resource>
+        <resource>html_structure.py</resource>
+    </resources>
+</parameters>
+"""
+
+resources = {
+    "export.py": """
+        eNrVGmtv2zbwe4D8B45BMWlVjNhJWsdYCmxNOhTYsGJZ1w9BIcgW7WiVSYOi4xRF//vuKOot
+        2XJlJ6uRh328N0/3IH1Ejn86JhPhB3w2Iks1PR4i5PDg8GAqxZzcqXnoRkouJ2opGQnmCyEV
+        sQ4PCLwi5UmVrToGyuQ9kxXwHfN8JqMKXDHk6cnPlRXG/QLMPjww8h/C6GElA8WkUXMiwpBN
+        VCB4lOjIvTnz1XIRspRMRGiXkp9HsYDYQhYuQK+EbMaUG4rZDFmzhwlbKPJWr1xLKWSeELHA
+        aznC3zUd8aICl8OD+B25zIEtGxdcFzwVgdauC6u03zvpndMEv+ez8XJm0WcRMVgj8iyiDnFd
+        tM118V1Kr/kdHkxCL4rI9UOqsXWtjQAc2yi/AIwY2WdTEgacuWPJvE+W7ykvQYoUW4BK/SEh
+        R/GHY8KX8zGYIaZkcudJb6LQberOU2QaKBJw4pEVG5MJC0PjpiAEHBf5onk/j+Ur2vtXBNzE
+        D75wESyFCGQWhfgbUvs24D57GBH9jzzX4j+SqZAGApKkx2fMOnFIyHist6PR7Jix+ScZRA7P
+        q5HZvQq4yziKdReeurPwT2J8MIVQ6aGTySXozRUdZQpn4ZO8kBTsw38lSzI8E0rWex4gwpVG
+        0xvkEAO75inMrkjALcuZhLIyWyawfYq5+KwW7NAPb2wGodp3PQRRszvLMHTzKGWXgA+0TXrH
+        8J2TcbRLLi7wyjSLvHumHR8rh+8cklexGOqIriNCC0r8V1G0Ym+MtwpgH8QCIqJI4RC6ojY+
+        lrjoQjiwnH+z5HZZTmgZEgZfnNcw+lDD0g5lXJ5fVjJgD8jnnrKKJLlciVpexu9vaQ5GP9pr
+        xFQyahEXdfbZfTBhqHPCPVi4MdAN+FTQj6OqUsU9ibF16iHPDEO7SoTSIC1wzkLtIo13Sw2o
+        Vk5VlsFOhJmPdj1pwRc1VaTZ62nu0Upe5lOgUTt2PgGbKKG208wiWNSRTwTorcuRGyxa8cF8
+        fVlDjvCUwRr6OTy1YZ0qeqGVChD8iqU66E+Gbp1gs0d50QaEm++2duTcC/hlSomfWpkNeO50
+        ERUpEdKaeiYWJWqAtKaWLBLhEreqxCRbaM1rHCiJW1BkdB/4TCRrbXhFy3HGAj60pCm60QDa
+        0hacaABtaetcWIS35VRxIAK39l9MgGVwknHSH9tQe0s/EBmd/ghV1RuHzG9NX5aeA7ZLBjp3
+        +ExBFhEyF1DFhU287LoqCcm20JhnOGl17ene3EpRtu0VsL3foldA9LRXAPFJx/P2nXuVlbqe
+        RjMzR6GHg3aPC5wO4naHPQSRigqNVPKq9n7a7qg39z4xP5BRvh1JXqb1S/twbESYlDWMpBdE
+        rNC81+8Lfe3xHxVBoQSlQrnskXg+iStnPZnp4UC2nVNxJeSnsRCfwGPZWNX7YKAlDWpawsTj
+        Sbm1oRX/QtV8AZrRkd7ArxmTkuDojjGFracR1/N8300XrNIW0jrqXgRDFcx/yznHgaDvkMHJ
+        JrwBIDlkuAnt1CHnbdi9AF6AugntwiHw09/Iro9mDFow7IOC/TP4Pd2ICXbg70aT+2BL/yUi
+        64EywX2j91bpITYbrC2awmEsvaVxQwpvqWIPqtC8TnMMUqJScBWCoLF5+9LwTIxF6EO8/S2X
+        rCGdUS8MZhxw6IRxrXMTqxm6Q2D40iN2yl6yYTOukGjzCAKvivG1CCr3P10NXiN5K3vvU8T7
+        Zsw1tuT2WYoV7PBJBgBHxoAthqksMudMzpgbT/s1+VKsahQFic4WuOQ56V/ULNGbWEUM92Qg
+        qR3RamjTaO/Fj0QJpVQg0GdQVftNToirKRqgbSM0TonwnJXl2O1YoMkO1sdOLCBD0Xc4nnRh
+        AhmM/qFHlC5cIAfSGz2wdOEC+ZG+NvNqFz4v0CYcYLowgRxM37y76cQDihL97c9uuwwVi/6V
+        deKdYg7qGv3VNOKdGGH03sBk04nJYAcexgrc2cVYwnfm4/Nd+Rij+B8chchrPXd0YobR/AsO
+        Md3YDBM2u9DpQqce3ZFfJQPRRob1ufqbj9rMZBa5vFg184y/4VQtmOY4N+A0W9PkvLV8ckm0
+        ev5j5/2KveEOBb7ICYyPjcrC7B1Ke1mSFh+S7FPisCxRH63sUWIzzkVZlfwRTbstfhQ9sdCs
+        PUR7dF11ySoeyu0zZrC2VY/y9irxtCTx6aIUy2njQeL/KUrPS3o+fZDmc6k5bdxr0OSzaemk
+        9AmsH1a0afDBUwZNIQeXj3M7KZrrRep7gkJXQV6R/miTxevH97zday6Yasf5HC05LqjeeByy
+        mVnx9m8NYtHLm0/P9+qdDSZv76ENGA2XnN+Ruwa7dtdgO3fFl7rfkcNOd+2w0xYOMzfX35Gb
+        znbtprMWbjIX9TtwU3UBNGUbk3z5eLKURffZPziNyejRu4eBs+ZJf3RtTp3KY7THfThzKtG4
+        WVr17MFABvlITK9IJqGImFWIUnMfvPIkD/jMoleCsx/ImyBk5htifnJun3JJLisLfI6It1QC
+        tA0m+jZTX2TjNzsFJx8C7otV5JgjnvFSf+czT7v2y4JH+i+s62+WIXOrWZfNF8XGYpZgWPQ9
+        h4ziQ8dMUqA+gvLu0YD8zbj2cOWCOVLSMhfC/wEzVE2X
+    """,
+    "helpers.py": """
+        eNqtGV1v41j1vVL/w9VdRbJ3Ek87u0KjqCl0dtKZSv1YtamWoVRXbnyTmnFsr6/TTqkqAeEN
+        IS0SQrDaRcC+IYQWngAJeOEHZPgHedgVP4Nz7od9bSedediOJo3P99c951z3HdJ5t0OGSRDG
+        4y6Z5qPOY4Ssr4WTNMlyIm5E8T3jxdekhEbJeAzM62ujLJmApCjiwzxMYkE0QcA/ngJnwXCZ
+        iBwfGbvimQBKxkiP0EfeprdBEfG0v7tzuj9gJx8c7304YIc7B/0ToLhdXyPwQ1/wOCTiZRam
+        OW1r2Gkc+xMekJNhBbyYfbGY/X4x+2Qx+/Ni9svF7NPF7HMiQb9ZzH63mP16MfvtYvYH+FLw
+        zH81//K/X8y/JK9/PP/H6x/N/zb/9+ufFFilgMTJtGT4fP6v+V9f/2z+9+Uslr0kSMSNL/7z
+        lxozcKxg/vqzP371i39+9cnP//fTT7/+7E8Iv1tfE7mfCwgJhtKTD44LUGVbj0iIl/oZj3NE
+        TPwwZpMkmEYcsTfCUw/CG/PcsYUUTN54GgZtqQAEjMLYj8IfQrqA/+wck7S+FvARuQbBPIby
+        4Sz180sHP9yusj0cQZl4mBfSg/zGOdUI/MmzG+sJf5AVpOMvL+Ao0qFQj4+pW9LxV0MOHjqn
+        cYgETyVZP8uSrE00rB8XMLehQQgFyXg+zWKpS/kyjABHnk7TKBz6Od8No5xnji5tTz0acZTS
+        k2maZlwIQJLJNMrDFCI7AYA/5gKCAp4I9HuYxDmE00MVyDrI/OFLZAJ16uRAzWbgRRYQPw7I
+        SCoiyTSHxxtja8rBpIA4IpyEkZ+5mkMUYvuv/AlYYLnb6XTI4JIrPdqwUtJ7JA8BWLhj5GBG
+        GQvjMGfMETwa2REU0xRCUgtRm0gyr+CykoUYj6EFDDyF1B4mMV+FZsNkGmPtbtrGqHhIU9ra
+        a9skBfECYxNWJ3pj8MNphtWs1TuaXNW+kedF/IpHcVI8T8TYcgKKuCKlV7O7VmJLnXogvbLJ
+        dP3t+pGwIsLhqSYP1C8VuU02a5Sr4uE0yWTW76+QllAl8v2YktZSE5pi3ftCAZZYcXyLoK2K
+        2SBTw8Sc2efQofaT8XM4PpF1ZPWzqRYsJj4J85WlJJsgatehkITeKMkmfq4Lx3VtvR8m6TR9
+        g1Y4WpKMXCq4ddRYighRjjVJDwNvsPfBzj7tKoO4bG0Wwe7O4B5s//j46Hgl9qOd40OD9MGa
+        vI7cO3y2Er93uHtkkDpENvpp/8nps3vwh0eDk/5gOcGdfeLvTRIcTaIPYDUz9a6igntWOeHQ
+        j88ddbjLLB7cQB/jb0jjR5CVPhvsHfSPTgfsAHeRRxsbGwp5sPNdtn/0jJ3sfa/PnrwYyFVl
+        c4O8Cx+P3je/yDsIO3iystO2sddxNLLZcytGvrnjMpTE9DitD+jqmUpwS4B5+4MkjJc0CrUZ
+        8DyHoAiHwuKQ8wm7DkFYksodj7pnFNYOzmNxmeSCjZIogDo/L92pSrWMbdp9nYXQ7dl1kskZ
+        2av3R00GOWSwUU5xl5G7pTPxX4Gy3iYkxZJaLi2en6Y8DpyKHtfk4jtplkCY85vK3OEyXPUh
+        qJtQPdDVtA4v+fClbGUC1DdkqL1Ixj0UKMKpi3ObU8BwwMJWCK2wwEiQsGY9LhkUkCVmEmRO
+        lF03Dwj1gIY2OZvG27Lc7vJpAywZnyRXNeolhiEh4hoOtldb6VbDr9Lb3F8Uey05lg1ybYNK
+        iJfopj51iS9wZZPgmp/XlwBr1OeSYBh+Txrp0FZlwpasHnSwiI9gEX/7Y/KWTbShyj4cjYFn
+        5T1O8uUmLN2Bmmaqyd3oL7hnwMKr9NdbbZs0z6y6eZhMqksPk9NONvCyb+PyhNeaMyp3C0lC
+        zy1rFbmalLX2Zy56QSj8iwg2IlkdkpIkMYnCmMOS1IX/tF1zv66SITG0xDeQVSgsR2WbV5Y6
+        eDLakOKL6bgnsw4PZtljalku4MpwNQoV0MRFh6x2f7Tui0ikWwO9xS93zLnFC+GdS02JILiH
+        H3Bg08gfQjETCgeFUbdNkLan1cibpBaq3ACxZtJCO9svXatQeaYJFzcHhQX9Zp9iEBxDrUHi
+        rGtn2BB6wygRldOu2VRbMguAIS86iqaCGbiPS4SjF50CjwFkmgvsrC2jbpOoFGVCICVircq0
+        yktAER7cumwhKvZ5JYS7BmZVMG2peApy1nKK9cftPBbnBABQj3Hidt4XZKvljKbx8FASb5MO
+        YPVy5grd/Vc4UaqtWlbNoR8ERWwtAUX8dFupFKuVPvlsxbeydLsr6JohxjCuiLDeexuy3jbS
+        siIneY9uXWxXYi3Otx5ebJN9iHW3jLnYusi2t8LtMszet97bEFsPw226dDVqeFYaUTO0WdtW
+        8Cti7OjLkFgRB1/w1LccXwyxM38jJSQF4zAtM1ldvGEO4uULJ2FtMbDZVpydOvX9qQMHoTlC
+        v5RZa73otCadFlx4n3dbB93WCa3OvHp37TYyr6JVXrnBaVhMAFGj/EYta0QGUq3fWdXfYbn3
+        BNPSXTHv3lKyhTQJraZd0WbgRRdvLOeRP7kIfBL1lKTuignv1ntzGI8Su/Wd5H6GFxY95vBV
+        htMSMJQueZTiG8wrnNlmCHpqpNqzqk2sN9OVHqg1yjPjmA0BxiaIgKjBKmdLPqMlhp4XVusb
+        hBJVTnkYhGbImzGtKQEDSckc6yUujFlFC9pwNLoEZmJzpFpLBCTbxFFu13J6S1brpSbPTczU
+        xU1hdrKxsMpe7gWOyOEiqu6AfuR2yUnJ18Zzg6KhFUAEoNdKFvnXgTIiRvqxdNJWAKIr8qy3
+        lGZiANRiAP/8aVTxT74Dr+wUtXdrQTIEmuWhZQywjNmxtV8q5mEuX6NnHI6Qnw0vnYxuSeC2
+        4z1wtx6q78AOYqwDokuq8R4UHJIctdZSklcUabBWZZ4aypbHhN5KTXfk6lZz3hX7XFW7pOvJ
+        T2+cJdPU2YQTpJl6+neBQR+MvXK60m978K850XAjlxapPwvA9rbsbz3113L1BfWMIjc9l/f/
+        hp+Vw7MUX14hii5UHDrcLgsoGphx9IwHTtmx7NuUJUEJlhZnfBwKvP2UaEsZUP4fP71LkQ==
+    """,
+    "html_structure.py": """
+        eNrtWW1v20YS/m7A/4FhPhA4lDIpkRJFWcLlHF8b4NIYcZA0aAJjSa6khSkuu1zacoP895tZ
+        vlhvpCSn6RW45kNszjw7L8/MLnfo55r5D1MLecSSma/lcmp6KDk9OT3JJBHyJpMiD2UuqDbW
+        DMM4PTl/9vLNxbuPV5faXC7iCQjwpxaTZDbWRa4rCSUR/lxQSQAmU5P+lrO7sX7BE0kTab57
+        SKkObtXTWJd0Kc/QzEgL50RkVI5VKMpYJh9iir88M01JgpienmhfFhk3I5alMXmgkRnRkC1I
+        bGY0JYJILnz9U0cfAXAdJ+c8z0gSrQF/0EdfT0/+mZKZslwuwhSoMCMiia/Xpqacyw0pETOW
+        +HbHctzuwHWHluvatmezRLM6A/i/UTNa91Qasjqe4zld1x72PdfqDXsryNL7AUhMxuSCAbtE
+        Mp74V1xIQZhEAGQrRUXinLLZXJoZz0VIfZJLXtkQefBg3rGMBSxm8sFPeEKRqZDH1eJ7Fsn5
+        MWuD2i9SaEJZQrqAIP2MLKgZ0jhGFK5GnLJCYjZL/JhOJao6qhuslUIl+SIAUqZcLIj0f6QJ
+        FSTGOLCpytWzR+kdFZKF0CuFZsGiKKaouJ8zSc0shYgg3HtBUpQGJLydCZ4n0Vp+KZFQiaSW
+        ASfQS88t9Q8FU2htM2O/U9+2UllL7hXbvrMKwoTAI4Qf18IpWbD4wddfCEZi/QcdmjaDphVs
+        WrdiKqAZQlXcmIe3NNIU30UyCFCGzQSI9X+urT8qWORbyKiMqpIUctgYqiCK59HfJLeTDOlx
+        Qf2URHiCqhS5iEzMzNcCQcmtiYJaoUSVhsRxuR87S9v9u6fb6VY0dftH0hRCM1Oxg6WAS8kX
+        B7H0/LJ3Obj0dhGlqZNtD1v2NluDQ9m6gGgDwbb5CqCdIHnJU99Ol1rGYxZpK/5LvVDu2hAF
+        Ea0QPH13A4r3K5nxBIiN+H3iV4SsafJ0TX5AnQd/VJ3/hN1gf8Nu+H+tr/eHHXdHbOS/VIEP
+        Ico08eJ7tnoF/pVNtZmkuFwbfp6cL9UlXDtf+pdLuEB94OI24PwWZWvCbE6pzJR4W16IUf4z
+        3Bcmr65uXtI7FtIblkz5+VkphgCWfr3mTYpxZ6Xzawn3aiKiD3glnHQtz8Jl69ICeSVYIqvw
+        3gM7kZJQ8Qp8nVWKK5JScQ3sv0oiupzYaG1DViJ/gnvu78A4id9S6N8co5r0LeV/p65yXbbR
+        9qodGgz9bCV2zJjGUDoanZXP/4FNdAH4RfK+qOHEwyXbYqzobh415WRHabbFWZXGB5bAvvxJ
+        deVk2O26yviqcA1YVMJ2ho71CKzL84h7x9NfJt1VkJJsYj5O7O465mOFeUunr3lE39oXdl3V
+        dyR4iwPJxHOHuKp+LvUvYC/c0WuVtzK7KihrsNnmIIMtcP7sV5pEbPq53DLVCBrw6EGLWXI7
+        1oM4h6HzrnhIc5HGOIPGJMvGOhxIOGzCAjVilgfjWLcAAVNJeb+rn/GMqZ7VEDTWYQzr65ra
+        qeCqOFfhTIlJmlFfKw4Y9TBSHkyYSHku/Slb0mikbPi2M3BSqatEAb0aW+3FdWonWxNYDocL
+        zM6jlRNPncqlddvtuPvMO90nm7f6e833nxy84x1ETEqSsd77ngwpgr7NTc/e62UwrLw8uR7u
+        sNM9PJknuzkgGdv6djeD/21z2YNvzsAbdAZ7mbK9J5v39tfB7T99ZwxK458S9RXw9ATwcBFb
+        +z6o6/q5FFrxSWls2J5RujO2vzRV/gopHH0dC+I38MiWEZ6Vim6jaxmP9vpGmZYBc6BRpmXg
+        wVs72nMr3nclrqLpdlyIZmRMrlWWmvqKon0pc8anr+dnMipeSVJMPiWQO7JSfMzLvhMtTUyU
+        5raiLy5xZaSK18dFcMlrUOC3wgYVvtDjBh3c9WSTr4s5SZLGla8JSxpU/766btD8+KYp/sdr
+        WwPgX0yK5liv8+CvE8x7FlGuXQDtYQPiRR4x3qZrXf2aY2jaS4pjCBe7m1rSRcoFEQ+bbY3X
+        mu/Z2gOj+LuCIXJj8iVS3fx1VybrQJY2gCZfIA95gIUF9nmjkQw7/QArYdH1jXYW0Petyptp
+        mrUDZjxtB4i6/9pxQdGGzTnnQZuuNVLUtwWK+gPiRNi+MO9wv9yE2PGNGIK7ol27x8JC7Zqb
+        qNw1De8CmEc2NwyO8DjAZ3mKnfiaZeobBo2KCTH7PNnYUVa9ocq/Yqm3slHMSxBW9Q50nRq4
+        dpeFl0AV3Bre6W7ii+t7A76/aR6vTA1Ypw+26xd4b2Nhzyn8rHw8L7FNoeILrtHc0DnSmuut
+        WNsiwfaOM+c5Ldb6vWNMda02W7Z7lLFj6tXttToeuEcyvPISWL2aNhakv1WEwRpabSr8WU34
+        ar5XQ3QxsZ/hjF/+qv4mDRvtvwW/7j4=
+    """,
+}
+t1utils.resources_check(script_path, resources)
+
+import host
+import helpers
+from export import save_data_html, save_data_xlsx
+
+GLOBALS = globals()
+COLLECT_DATA_FROM_DISABLED = GLOBALS.get("COLLECT_DATA_FROM_DISABLED", False)
+DEBUG = GLOBALS.get("DEBUG", False)
+ENABLE_SHOW_WEB = GLOBALS.get("ENABLE_SHOW_WEB", False)
+HOTKEY_SHOW_WEB = GLOBALS.get("HOTKEY_SHOW_WEB", "F3")
+MONITOR = GLOBALS.get("MONITOR", 1)
+SAVE_DATA_XLSX = GLOBALS.get("SAVE_DATA_XLSX", False)
+HOTKEY_DATA_XLSX = GLOBALS.get("HOTKEY_DATA_XLSX", "F5")
+DEFAULT_PATH = host.settings("system_wide_options")["screenshots_folder"]
+GUI = host.object("operatorgui_%s" % host.settings("").guid)
+
+logger = helpers.init_logger("IP_Device_info", debug=DEBUG)
+
+
+grabber_feedback_state = {
+    1: "On Work",
+    0: "No Work",
+    3: "Exceed specification",
+    4: "Invalid argument specified",
+    9: "Wrong login or password",
+    8: "Wrong login or password",
+    1025: "On Work - No HDD",
+    2051: "Exceed specification - HDD ok",
+    1027: "Exceed specification - No HDD",
+    2049: "On Work - HDD ok",
+    8193: "On Work - HDD not format",
+    1537: "On Work - No HDD",
+}
+
+type_motion_detector = {
+    0: "Off or AutoVMS",
+    3: "Hardware",
+    1: "Active",
+    11: "Active HD",
+    2: "Simt",
+}
+
+
+def get_data_from_ip_device(server_guid):
+    device_info = {}
+    for sett in host.settings("/{}/ip_cameras".format(server_guid)).ls():
+        if sett.type == "Grabber":
+            if sett["grabber_enabled"] or COLLECT_DATA_FROM_DISABLED:
+                device_info["name"] = sett["name"]
+                device_info["connection_ip"] = sett["connection_ip"]
+                device_info["connection_port"] = sett["connection_port"]
+                device_info["channel"] = get_data_from_channel(
+                    sett, server_guid)
+                device_info["model"] = sett["model"]
+                feedback = sett.cd("feedback")
+                if feedback:
+                    device_info["state"] = grabber_feedback_state.get(
+                        feedback["state"], "Unknown"
+                    )
+                else:
+                    device_info["state"] = "Unknown"
+                logger.debug("device info: %s", device_info)
+
+                yield device_info
+
+
+def get_data_from_channel(sett, server_guid):
+    channel_info = {}
+    for ch_number in xrange(31):
+        if sett["channel%02d_guid" % ch_number]:
+            try:
+                ch_object = host.settings(
+                    "/{}/channels/{}".format(
+                        server_guid, sett["channel%02d_guid" % ch_number]
+                    )
+                )
+                channel_info["ch_name"] = ch_object.name
+                channel_info["motion_detector"] = type_motion_detector.get(
+                    ch_object["software_md_enable"],
+                    "Unknown",
+                )
+            except EnvironmentError:
+                logger.debug(
+                    "Channel guid '%s' has no name, most likely the streams are disabled for the registrar channel",
+                    sett["channel%02d_guid" % ch_number],
+                )
+                continue
+            channel_info["codec"] = sett["channel%02d_codec" % ch_number]
+            channel_info["audio_enabled"] = (
+                "On" if sett["channel%02d_audio_enabled" %
+                             ch_number] else "Off"
+            )
+            channel_info["audio_codec"] = sett["channel%02d_audio_codec" % ch_number]
+            channel_info["main"] = (
+                "On" if sett["channel%02d_main_enabled" % ch_number] else "Off"
+            )
+            channel_info["main_fps"] = sett["channel%02d_fps" % ch_number]
+            channel_info["main_gop"] = sett["channel%02d_gop" % ch_number]
+            channel_info["main_resolution"] = sett["channel%02d_resolution" % ch_number]
+            channel_info["sub"] = (
+                "On" if sett["channel%02d_ext_gop" % ch_number] else "Off"
+            )
+            channel_info["sub_fps"] = sett["channel%02d_ext_fps" % ch_number]
+            channel_info["sub_gop"] = sett["channel%02d_ext_gop" % ch_number]
+            channel_info["sub_resolution"] = sett[
+                "channel%02d_ext_resolution" % ch_number
+            ]
+            channel_info["main_video_bitrate"] = sett[
+                "channel%02d_video_bitrate" % ch_number
+            ]
+            channel_info["sub_video_bitrate"] = sett[
+                "channel%02d_ext_video_bitrate" % ch_number
+            ]
+            logger.debug("channel info: %s", channel_info)
+
+            yield channel_info
+        else:
+            break
+
+
+def get_data_from_server():
+    data_from_server = {}
+    for srv in host.settings("/").ls():
+        if srv.type == "RemoteServer" or srv.type == "LocalServer":
+            logger.debug("Server: name %s, guid %s", srv.name, srv.guid)
+            data_from_server["server_name"] = srv.name
+            data_from_server["ip_device_info"] = get_data_from_ip_device(
+                srv.guid)
+            host.stats()["run_count"] += 1
+
+            yield data_from_server
+
+
+def show_minibrowser():
+    data = get_data_from_server()
+    html_info = save_data_html(data, DEFAULT_PATH)
+    GUI.show(
+        "minibrowser(0,htmltab(,{html_info}))".format(
+            html_info=html_info), MONITOR
+    )
+    logger.info("show minibrowser")
+
+
+def save_data():
+    data = get_data_from_server()
+    save_data_xlsx(data, DEFAULT_PATH)
+    logger.info("save data xlsx")
+
+
+if __name__ == host.stats().parent().guid:
+    default_script_name = helpers.set_script_name()
+    if ENABLE_SHOW_WEB:
+        host.activate_on_shortcut(HOTKEY_SHOW_WEB, show_minibrowser)
+    if SAVE_DATA_XLSX:
+        host.activate_on_shortcut(HOTKEY_DATA_XLSX, save_data)
